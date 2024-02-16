d110c2f961b8eacde9fc218b122683818951c9a9
Tomasnuin <tomasnuingas@gmail.com>
2024-02-16 19:47:43 -0300

    Applicacion de React creada

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Main from './components/Inicio'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+      <Main></Main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Inicio.jsx
@@ -0,0 +1,13 @@
+import NavBar from './NavBar'
+import Main from './Main'
+
+const Inicio = () => {
+    return (
+        <>
+            <NavBar></NavBar>
+            <Main></Main>
+        </>
+    )
+}
+
+export default Inicio
