817213049ec2cb3247dfa071efc5acbffc8ca0cb
Santiago Nuñez Ingas <santiago.nunez.ingas@gmail.com>
2024-02-20 20:25:58 -0300

    Rutas agregadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { Route, Routes } from 'react-router';
 import './App.css'
-import Inicio  from './components/Inicio';
-
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Inicio from './components/Inicio';
+import Registro from './components/Registro/Registro';
 
 function App() {
 
   return (
-    <>
-      <Inicio></Inicio>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} >
+          <Route index element={<Inicio />} ></Route>
+          <Route path="registro" element={<Registro />} ></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
