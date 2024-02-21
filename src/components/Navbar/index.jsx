3a18d938df4d89dd9870a4e88c5de6226b6a5d1e
Tomás Nuñez Ingas <tomasnuingas@gmail.com>
2024-02-21 20:48:45 -0300

    botones ocultados en NavBar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -15,17 +15,19 @@ const NavBar = ({setLogin}) => {
                         className="texto-blanco nav-texto nav-custom-link" 
                         onClick={()=>setLogin(false)} to="/"> Inicio</Link>
                     </li>
-                    <li>
+                    {/* <li>
                         <Link href="#menu" 
                         className="texto-blanco nav-texto nav-custom-link" 
-                        /* TOMAS: hasta que este terminado el menú, clickear este botón te va a llevar a el login */
+                        // TOMAS: hasta que este terminado el menú, clickear este botón te va a llevar a el login
                         onClick={()=>setLogin(true)} to="/"> Menú</Link>
-                    </li>
+                    </li> */}
                 </ul >
                 <Link href="#login" 
                 className="texto-blanco background-primary-yellow rounded nav-texto nav-boton boton-primary" onClick={()=>setLogin(true)} to="/"> Iniciar Sesión</Link>
+                {/* TOMAS: Botón coulto hasta que la pagina de registro este completa 
                 <Link href="#registro" 
                 className="texto-blanco background-night-yellow rounded nav-texto nav-boton boton-night" to="/registro" > Registrarme</Link>
+                 */}
             </div>
 
         </nav >
