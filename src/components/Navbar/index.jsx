2371b3de11778bfa001df8e5e7c6a3d69dadc46e
Tomás Nuñez Ingas <tomasnuingas@gmail.com>
2024-02-21 19:33:37 -0300

    NavBar, la marca ya no es un boton. NavBar css, colores ahora usan variables

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const NavBar = ({setLogin}) => {
     return (
         <nav id="navbar">
-            <Link href="#inicio" className="texto-blanco marca-underline" to="/" onClick={()=>setLogin(false)}>
+            <div className="texto-blanco marca-underline">
                 <span className="texto-marca-custom">Custom</span>
                 <span className="texto-marca-slice texto-amarillo">Slice</span>
-            </Link>
+            </div>
             <div id="nav-links">
                 <ul id="nav-ulist">
                     <li>
@@ -18,14 +18,14 @@ const NavBar = ({setLogin}) => {
                     <li>
                         <Link href="#menu" 
                         className="texto-blanco nav-texto nav-custom-link" 
-                        /* TOMAS: hasta que este el menú clickear este botón te va a llevar a el login */
+                        /* TOMAS: hasta que este terminado el menú, clickear este botón te va a llevar a el login */
                         onClick={()=>setLogin(true)} to="/"> Menú</Link>
                     </li>
                 </ul >
                 <Link href="#login" 
-                className="texto-blanco background-dark-yellow rounded nav-texto nav-boton" onClick={()=>setLogin(true)} to="/"> Iniciar Sesión</Link>
+                className="texto-blanco background-primary-yellow rounded nav-texto nav-boton boton-primary" onClick={()=>setLogin(true)} to="/"> Iniciar Sesión</Link>
                 <Link href="#registro" 
-                className="texto-blanco background-night-yellow rounded nav-texto nav-boton" to="/registro" > Registrarme</Link>
+                className="texto-blanco background-night-yellow rounded nav-texto nav-boton boton-night" to="/registro" > Registrarme</Link>
             </div>
 
         </nav >
