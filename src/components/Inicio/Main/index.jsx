e3cb7d0af20fe48a0b7997e15cd8919b069a40c5
Caseres Marina <marinacaseres1997@gmail.com>
2024-02-26 19:58:39 -0300

    Agregados cambios para Deploy.

--- src/components/Inicio/Main/index.jsx
+++ src/components/Inicio/Main/index.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from 'react-router-dom';
 import './index.css'
 import  "react-bootstrap"
 
+import pizza_png from '../../../assets/pizza.png'
+
 const Main = ({login, openLogin}) => {
     console.log(login);
     const navigate = useNavigate();
@@ -17,7 +19,7 @@ const Main = ({login, openLogin}) => {
     if(login){
         return(
             <section name="login" className='form-background'>
-                <img className="background-pizza z-2" src='/src/assets/pizza.png'></img>
+                <img className="background-pizza z-2" src={pizza_png}></img>
                 <svg className="background-shape" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1277.08 767.46">
                     <path d="m317.33,3.41c-34.36,7.41-92.12,24.57-149.75,68.07-84.24,63.58-117.27,144.98-134.43,188.89C20.19,293.52-4.83,359.61.82,447.55c4.48,69.83,26.17,120.31,39.14,149.75,34.65,78.69,83.56,134.31,122.52,170.17h1114.6V0c-319.92,1.14-639.83,2.27-959.75,3.41Z" fill="currentColor"/>
                 </svg>
@@ -38,7 +40,7 @@ const Main = ({login, openLogin}) => {
     } else {
         return (
             <main>
-                <img className="background-pizza z-1" src='/src/assets/pizza.png'></img>
+                <img className="background-pizza z-1" src={pizza_png}></img>
                 <div className='position-absolute welcome'>
                     <h1 className="text-start text-white">Decora tu pizza a tu gusto</h1>
                     <p className="text-start texto-blanco parrafo-inicio z-3">En CustomSlice crea la pizza de tus sueños, ¡justo como te gusta! Sumérgete en un mundo de infinitas posibilidades y sabores, donde cada bocado es una experiencia personalizada. </p>
